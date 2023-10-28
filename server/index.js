6d54d0c61a7d64db8103576d2103307f4ce6e71a
redietgash21 <redietgash21@gmail.com>
2023-10-28 20:24:53 +0300

    server index update

--- server/index.js
+++ server/index.js
@@ -5,109 +5,121 @@
 
 
 const express = require ('express')
-const mysql = require ('mysql2')
+const app = express()
+const morgan=require('morgan')
+const bodyParser=require('body-parser')
 const cors = require("cors")
 
+//import routers
+const todo = require('./routers/todoRouter')
+
+//middle ware
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: "5mb"}))
+app.use(bodyParser.urlencoded({
+   limit: "5mb",
+   extended: true
+}))
+app.use(cors());
+
+//route middleware
+app.use('/api', todo)
+
+//port
+const port = 5000;
+app.listen(port, ()=>{
+   console.log(`server runing on port ${port}`)
+})
+
+// require('dotenv').config()
 
 
-require('dotenv').config()
+// const PORT = process.env.port || 5000
 
-const app = express()
-const PORT = process.env.port || 5000
+// app.use(express.json())
+// app.use(cors())
 
-app.use(express.json())
-app.use(cors())
 
-const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'PFH#23kgrw9',
-    database: 'todoapp'
- });
- db.connect(err=>{
-    if(err) console.log("Error while connecting to database: ",err)
-    else console.log("Server is running...");
- })
 
- app.post('/save', (req, res)=>{
-    // console.log("========",req.body,"===========")
-    const id= req.body.id;
-    const todoText= req.body.todoText;
-    const statuss=req.body.statuss;
-    let tableEmployee = `
-        insert into todoTable( 
-        id,todoText,statuss) 
-        values(?,?,?)`;
+//  app.post('/save', (req, res)=>{
+//     // console.log("========",req.body,"===========")
+//     const id= req.body.id;
+//     const todoText= req.body.todoText;
+//     const statuss=req.body.statuss;
+//     let tableEmployee = `
+//         insert into todoTable( 
+//         id,todoText,statuss) 
+//         values(?,?,?)`;
   
-    db.query(tableEmployee,
-       [ id, todoText, statuss], (err, result)=>{
-        if(err){
-           console.log(err);
-        }
-        else{
-           console.log("add to table");
-           res.send("add to table");
-        }
-       }
-  );
+//     db.query(tableEmployee,
+//        [ id, todoText, statuss], (err, result)=>{
+//         if(err){
+//            console.log(err);
+//         }
+//         else{
+//            console.log("add to table");
+//            res.send("add to table");
+//         }
+//        }
+//   );
   
-  });
-  app.post("/update",(req,res)=>{
+//   });
+//   app.post("/update",(req,res)=>{
  
-    console.log(req.body);
-    const id= req.body.id;
-    const todoText= req.body.todoText;
-    const statuss=req.body.statuss;
-      let tableLaptop = `
-         update todoTable set 
-         todoText='${id}', 
-         statuss='${todoText}'
-         where id='${statuss}'`;
-      db.query(tableLaptop,
-         [id, todoText,statuss], (err, result)=>{
-          if(err){
-             console.log(err);    
-          }
-          else{
-             console.log(result.affectedRows," updated");
-             res.send("values updated");
-          }
-         }
-   );
-   })
-   app.post("/delete",(req,res)=>{
+//     console.log(req.body);
+//     const id= req.body.id;
+//     const todoText= req.body.todoText;
+//     const statuss=req.body.statuss;
+//       let tableLaptop = `
+//          update todoTable set 
+//          todoText='${id}', 
+//          statuss='${todoText}'
+//          where id='${statuss}'`;
+//       db.query(tableLaptop,
+//          [id, todoText,statuss], (err, result)=>{
+//           if(err){
+//              console.log(err);    
+//           }
+//           else{
+//              console.log(result.affectedRows," updated");
+//              res.send("values updated");
+//           }
+//          }
+//    );
+//    })
+//    app.post("/delete",(req,res)=>{
  
-    console.log(req.body);
-      const id= req.body.id;
-      let tableLaptop = `
-         delete from todoTable 
-         where id='${id}'`;
-      db.query(tableLaptop,
-         [id], (err, result)=>{
-          if(err){
-             console.log(err);
+//     console.log(req.body);
+//       const id= req.body.id;
+//       let tableLaptop = `
+//          delete from todoTable 
+//          where id='${id}'`;
+//       db.query(tableLaptop,
+//          [id], (err, result)=>{
+//           if(err){
+//              console.log(err);
             
-          }
-          else{
-             console.log(result.affectedRows," deleted");
-             res.send("values deleted");
-          }
-         }
-   );
-   })
-  app.get('/', (req,res)=>{
-    const empl="select * from todoTable";
-    db.query(empl, (err,result)=>{
-         if(err){
-          console.log(err);
-         }
-         else{
-          res.send(result);
-          console.log(result)
+//           }
+//           else{
+//              console.log(result.affectedRows," deleted");
+//              res.send("values deleted");
+//           }
+//          }
+//    );
+//    })
+//   app.get('/', (req,res)=>{
+//     const empl="select * from todoTable";
+//     db.query(empl, (err,result)=>{
+//          if(err){
+//           console.log(err);
+//          }
+//          else{
+//           res.send(result);
+//           console.log(result)
          
-         }
-    });
- })
+//          }
+//     });
+//  })
 
 
-app.listen(PORT, () => console.log(`server runing on : ${PORT}`))
+// app.listen(PORT, () => console.log(`server runing on : ${PORT}`))
