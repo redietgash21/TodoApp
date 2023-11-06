2f256acc56cead188d15fe5f2315fec148c0fdc1
redietgash21 <redietgash21@gmail.com>
2023-11-06 22:18:57 +0300

    add edit and delete router to backend

--- server/routers/todoRouter.js
+++ server/routers/todoRouter.js
@@ -6,10 +6,12 @@
 
 const express = require ('express');
 const router = express.Router();
-const { insertTodoTable, showTodos} = require('../controllers/todoController');
+const { insertTodoTable, showTodos, editTodoTable, deleteTodoList} = require('../controllers/todoController');
 
 //routes
 router.post('/insert',insertTodoTable);
+router.post('/edit',editTodoTable);
+router.post('/delete',deleteTodoList);
 router.get('/showTodos',showTodos);
 
 
